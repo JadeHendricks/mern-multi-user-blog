6043bdd869016b91564befb97e6f0cc8a2ca2a6a
jadehendricks <hendricksjade@gmail.com>
2020-07-21 20:40:47 +0200

    added in actions and inital state for posts

--- client/src/context/postContext/PostReducer.js
+++ client/src/context/postContext/PostReducer.js
@@ -1,18 +1,30 @@
-import { GET_POST, GET_ALLPOSTS, CREATE_POST, EDIT_POST, POST_ERROR  } from '../types';
+import { GET_POST, GET_ALL_POSTS, CREATE_POST, EDIT_POST, DELETE_POST, POST_ERROR  } from '../types';
   
 export default ( state, action ) => {
     switch (action.type) {
-        case GET_ALLPOSTS: 
+        case GET_ALL_POSTS: 
             return {
-
+                ...state,
+                posts: action.payload,
+                loading: false
             }
         case GET_POST: 
             return {
-
+                ...state,
+                post: action.payload,
+                loading: false
             }
         case CREATE_POST: 
             return {
-
+                ...state,
+                posts: [action.payload, ...state.posts],
+                loading: false
+            }
+        case DELETE_POST: 
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== action.payload),
+                loading: false
             }
         case EDIT_POST: 
             return {
@@ -20,7 +32,8 @@ export default ( state, action ) => {
             }  
         case POST_ERROR: 
             return {
-
+                ...state,
+                error: action.payload
             }   
         default: 
         return state;
--- client/src/context/postContext/PostState.js
+++ client/src/context/postContext/PostState.js
@@ -1,24 +1,98 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer } from "react";
 import PostContext from './PostContext';
 import PostReducer from './PostReducer';
+import { toast } from 'react-toastify';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
-import { GET_POST, GET_ALLPOSTS, CREATE_POST, EDIT_POST, POST_ERROR  } from '../types';
+import { GET_ALL_POSTS, GET_POST, CREATE_POST, DELETE_POST, EDIT_POST, POST_ERROR  } from '../types';
 
 const PostState = props => {
+
     const initialState = {
-
+        posts: [],
+        post: null,
+        loading: true,
+        error: null
     };
-
-    useEffect(() => {
-
-    }, []);
-
+    
     const [state, dispatch] = useReducer(PostReducer, initialState);
+
+    const getAllPosts = async () => { 
+        try {
+            const res = await axios.get('/api/user');
+            dispatch({ 
+                type: GET_ALL_POSTS, 
+                payload: res.data.posts
+            });
+        } catch (err) {
+            dispatch({
+                type: POST_ERROR,
+                payload: err.response.message
+            });
+        }
+    }
+
+    const getPost = async (id) => {
+        try {
+            const res = await axios.get(`/api/user/${id}`);
+            dispatch({ 
+                type: GET_POST, 
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: POST_ERROR,
+                payload: err.response.message
+            });
+        }
+    }
+
+    const createPost = async (title, tag, description, user) => { 
+        const config = { headers: {'Content-Type': 'application/json'} };
+        const body = JSON.stringify({ title, tag, body, user });
+        try {
+            const res = await axios.post('/api/user', body, config);
+            dispatch({
+                type: CREATE_POST,
+                payload: res.data.post
+            });
+            toast.success(res.data.message);
+        } catch (err) {
+            dispatch({
+                type: POST_ERROR,
+                payload: err.response.message
+            });
+        }
+    }
+
+    const deletePost = async (id) => { 
+        try {
+            await axios.delete(`/api/post/${id}`); 
+            dispatch({ 
+                type: DELETE_POST, 
+                payload: id 
+            });
+        } catch (err) {
+            dispatch({
+                type: POST_ERROR,
+                payload: err.response.message
+            });
+        }
+    }
+
+    const editPost = async () => { console.log('edit-post') }
 
     return (
         <PostContext.Provider value={{
-
+            posts: state.posts,
+            post: state.post,
+            loading: state.loading,
+            error: state.error,
+            getAllPosts,
+            getPost,
+            createPost,
+            editPost,
+            deletePost
         }}>
           { props.children }
         </PostContext.Provider>
