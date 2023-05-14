bea45b2b466a6043d37ac201bfbb3a501fdf0822
Jade <hendricksjade@gmail.com>
2023-05-14 23:13:40 +0200

    fix(build): fixed the build from failing.

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ const connectDB = require('./db');
 
 connectDB();
 
-if (process.env.PORT) {
-   app.listen(port, () => console.log(`Server running on port:${process.env.PORT}`))
-}
+app.listen(port, () => console.log(`Server running on port:${port}`))
 
 //Middleware
 app.use(morgan('dev'));
@@ -52,5 +50,3 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    });
 }
-
-module.exports = app;
